76a0cbbd2d2226c7713816d7ef8f0e340145d650
SkyeRenda <skyerenda@hotmail.com>
2022-12-14 21:12:39 +0200

    ready to deploy

--- index.js
+++ index.js
@@ -51,3 +51,13 @@ app.get("/download", async (req, res) => {
   const url = await getDownloadURL(storageRef);
   res.status(200).json(url);
 });
+
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      name: "name of your app",
+      version: "0.1.0",
+    },
+  });
+});
