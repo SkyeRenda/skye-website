5ae41f7fe8a8e5910797c37cbd5bf8297ec80cfd
SkyeRenda <skyerenda@hotmail.com>
2022-12-14 21:21:17 +0200

    ready to deploy

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ if (process.env.PORT === "production") {
   //set static folder
   app.use(express.static("client/build"));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  //});
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
