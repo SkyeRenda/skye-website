2b276fe28ef84cdff847e36edd9483686915f6d3
SkyeRenda <skyerenda@hotmail.com>
2022-12-15 21:07:01 +0200

    made app responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Desktop } from "./components/desktop/desktop.component";
+import { Mobile } from "./components/mobile/mobile.component.jsx";
+import { useMediaQuery } from "react-responsive";
 
 function App() {
   const response = "";
 
   const [url, setUrl] = useState();
+  const [windowSize, setWindowSize] = useState(getWindowSize());
 
   const retrieveUrl = async () => {
     const response = await axios.get("/download");
@@ -17,24 +21,39 @@ function App() {
     retrieveUrl();
   }, []);
 
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  const isMobileDevice = useMediaQuery({
+    query: "(min-device-width: 450px)",
+  });
+
+  const isDesktop = useMediaQuery({
+    query: "(min-device-width: 1200px)",
+  });
+
   return (
     <div className="App">
       <div className="App-header">
-        <div className="Front-Page">
-          <div className="centre-piece">
-            <h1 className="Hello-Header">Hello</h1>
-            <div>
-              <h2>Skye Renda</h2>
-              <h2>Software Developer</h2>
-            </div>
-            <a href={url} target="_blank" download>
-              <button className="Cv-Download">See my CV</button>
-            </a>
-          </div>
-        </div>
+        {windowSize.innerWidth > "600" && <Desktop url={url} />}
+        {windowSize.innerWidth <= "600" && <Mobile url={url} />}
       </div>
     </div>
   );
 }
 
 export default App;
+
+function getWindowSize() {
+  const { innerWidth, innerHeight } = window;
+  return { innerWidth, innerHeight };
+}
